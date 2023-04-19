d3e421c1aa8a957ee42ba9b8e837618569e202d7
Oleg <ovt1966@gmail.com>
2023-04-19 08:59:44 +0300

    Change files2

--- src/test/java/MonthServiceTest.java
+++ src/test/java/MonthServiceTest.java
@@ -1,39 +1,22 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import ru.netology.services.MonthService;
 
 public class MonthServiceTest {
-    @Test
-    void testCalculateMonth1() {
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/month.csv")
+    void testCalculateMonth1(int expected, int income, int expenses, int threshold) {
         MonthService service = new MonthService();
 
-        // подготавливаем данные:
-        int income = 10_000;
-        int expences = 3_000;
-        int threshold = 20_000;
-        int expected = 3;
 
-        // вызываем целевой метод
-        int actual = service.calculate(income, expences, threshold);
+//        вызываем целевой метод
+        int actual = service.calculate(income, expenses, threshold);
 
         // производим проверку (сравниваем ожидаемый и фактический результат)
         Assertions.assertEquals(expected, actual);
     }
 
-    @Test
-    void testCalculateMonth2() {
-        MonthService service = new MonthService();
 
-        // подготавливаем данные:
-        int income = 100_000;
-        int expences = 60_000;
-        int threshold = 150_000;
-        int expected = 2;
-
-        // вызываем целевой метод
-        int actual = service.calculate(income, expences, threshold);
-
-        // производим проверку (сравниваем ожидаемый и фактический результат
-        Assertions.assertEquals(expected, actual);
-    }
 }
